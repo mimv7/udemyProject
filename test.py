523f4e88a05c8e2b806037f78a669140134a7274
Roman <meleshin.r@yandex.ru>
2021-09-20 13:23:41 +0300

    next

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
-backes = ['honda','yamaha','suzuki']
-backes.append('ducaty')
-
+country = ['Usa','Russia','Akraine','Turkey','Banada','1']
+country.sort()
+for i in country:
+    print(f"{i} is country")
+for i in country:
+    print('i love pizza')
+print('in Italia')
